2f8f2e57a730af6cc83e11b759d3a943f10cb05f
busari007 <123savagewilly@gmail.com>
2024-06-08 19:13:52 +0100

    Done

--- src/ImageUpload.jsx
+++ src/ImageUpload.jsx
@@ -1,54 +1,41 @@
-import React, { useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { storage } from './firebaseConfig'; // Import the storage object
+import './App.css';
 
 const ImageUpload = ({ onUpload }) => {
-  const [file, setFile] = useState(null);
   const fileInputRef = useRef(null);
 
-  const onChange = e => {
-    const selectedFile = e.target.files[0];
-    if (selectedFile) {
-      setFile(selectedFile);
-    }
-  };
-
-  const onSubmit = async e => {
-    e.preventDefault();
-
-    if (!file) {
+  const handleUpload = async () => {
+    const selectedFile = fileInputRef.current.files[0];
+    if (!selectedFile) {
       alert('Please select a file\nVeuillez sélectionner un fichier');
       return;
     }
 
-    const storageRef = ref(storage, `images/${file.name}`);
-    await uploadBytes(storageRef, file);
+    const storageRef = ref(storage, `images/${selectedFile.name}`);
+    await uploadBytes(storageRef, selectedFile);
     const url = await getDownloadURL(storageRef);
 
     console.log('Uploaded image URL:', url);
     alert('Image successfully uploaded\nImage téléchargée avec succès');
     onUpload(); // Trigger the reload of images
-    setFile(null); // Reset the file state
-    if (fileInputRef.current) {
-      fileInputRef.current.value = ''; // Clear the file input field
-    }
+    fileInputRef.current.value = ''; // Clear the file input field
   };
 
   return (
     <div className="container">
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <input
-            type="file"
-            className="form-control-file"
-            id="fileInput"
-            name="image"
-            onChange={onChange}
-            ref={fileInputRef}
-          />
-        </div>
-        <button type="submit" className="upload">Upload / Télécharger</button>
-      </form>
+      <label className="upload" htmlFor="fileInput">
+        Upload / Télécharger
+      </label>
+      <input
+        type="file"
+        id="fileInput"
+        name="image"
+        onChange={handleUpload}
+        ref={fileInputRef}
+        style={{ display: 'none' }} // Hide the input field
+      />
     </div>
   );
 };
